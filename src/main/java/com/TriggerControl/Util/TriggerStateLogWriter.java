bb6057f032640a85d9a7cd97cfab81b138b205d4
davidsudobit <davidmanson474@gmail.com>
2025-01-31 17:11:16 +0530

    Added GenAI promts used for unit test

--- src/main/java/com/TriggerControl/Util/TriggerStateLogWriter.java
+++ src/main/java/com/TriggerControl/Util/TriggerStateLogWriter.java
@@ -5,7 +5,6 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
